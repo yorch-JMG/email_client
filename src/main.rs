2214d3944528738700185f05c695f10f8e79f6d7
Jorge <jorgeMGdev@gmail.com>
2022-04-20 11:07:02 -0700

    print all emails with status colored

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::{io::stdin};
+use colored::Colorize;
+use reqwest::Error;
 use serde::{Serialize, Deserialize};
 
 
@@ -11,12 +14,42 @@ struct Email {
     read: bool
 }
 
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let resp = reqwest::get("http://localhost:3000/emails")
-        .await?
-        .json::<Vec<Email>>()
-        .await?;
-    println!("{}", resp[0].emailBody);
-    Ok(())
+fn print_status(read: bool){
+    let mut read_color : String;
+    if read {
+        read_color = "Read".green().to_string();
+        println!("Status: {}",read_color)
+    }
+    read_color = "Read now".red().to_string();
+    println!("Status: {}",read_color)
 }
+
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    loop {
+        println!("Welcome to this email client, please select an option from below:");
+        println!("1. Get all emails");
+        println!("2. Send an email");
+        println!("3. Read an email");
+
+        let mut user_input = String::new();
+        stdin().read_line(&mut user_input).expect("No value found");
+            if user_input.contains("1"){
+                let response = reqwest::get("http://localhost:3000/emails").await?;
+                let emails: Vec<Email> = response.json().await?;
+                for (i, email) in emails.iter().enumerate() {
+                    println!("{}. {}", &i+1, email.title);
+                    print_status(email.read);
+                }
+            }
+            if user_input.contains("1"){
+            }
+            if user_input.contains("1"){
+            }
+            else {
+                break;
+            }
+        };
+        Ok(())
+    }
